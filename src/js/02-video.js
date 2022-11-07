d83feb2c711d120de9e44b4a2feb8ddf6375ecc6
Ivan Onofriichuk <ivanonofrijcuk@gmail.com>
2022-11-07 23:49:00 +0200

    add 2 and 3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay(ev) {
+  localStorage.setItem(LOCAL_KEY, ev.seconds);
+}
+
+setCurrentTime();
+function setCurrentTime() {
+  const savedTime = localStorage.getItem(LOCAL_KEY);
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+}
 
